a1c0b1386e4aac025286164e6fad5cda7ab5463b
YoungTeam <youngteam517@gmail.com>
2024-06-29 02:03:21 +0800

    add csamm

---
+++ 05/CSAMM.sol
@@ -0,0 +1,21 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CSAMM{
+    IERC20 public immutable token0;
+    IERC20 public immutable token1;
+
+    uint public reserve0;
+    uint public reserve1;
+
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+
+    constructor(address _token0,address _token1){
+        token0 = IERC20(_token0);
+        token1 = IERC20(_token1);
+    }
+    
+}
